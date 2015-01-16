539a650acba47b72a246d21f5b01ff12773080d4
Nikit Saraf <nikit.saraf@gmail.com>
2015-01-16 11:39:46 +0530

    Initial Commit

---
+++ tapkuChalega/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tapkuChalega
+//
+//  Created by Nikit Saraf on 16/01/15.
+//  Copyright (c) 2015 Areysun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tapkuChalega/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tapkuChalega
+//
+//  Created by Nikit Saraf on 16/01/15.
+//  Copyright (c) 2015 Areysun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
